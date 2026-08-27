9048c34a261dfa9f3312e3bbcd5bd7c44a036189
Jack Gittins <jack.gittins@grilld.com.au>
2026-08-27 11:35:49 +1000

    added remi chip

---
+++ src/lib/components/remi.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import remiImage from '$lib/assets/remi.png';
+</script>
+
+<button class="width-16">
+	<img alt="Remi" src={remiImage} class="chip-container h-auto w-16" />
+</button>
+
+<style>
+	.chip-container {
+		position: fixed;
+		bottom: 60px;
+		z-index: 1000;
+		right: 15px;
+	}
+</style>
